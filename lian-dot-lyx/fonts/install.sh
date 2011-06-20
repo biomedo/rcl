98e285421762bb351c5cbcaf0077c120342d06aa
WeiDong.Lian <WeiDong.Lian@gmail.com>
2011-06-20 09:01:04 +0000

    remove *.elc in autex path and also remove the big chinese adobe fonts, now it can be downloaded from the downloads table in rcl google code

--- lian-dot-lyx/fonts/install.sh
+++ lian-dot-lyx/fonts/install.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-# Time-stamp: <2011-06-18 00:44:38 Saturday by lian>
+# Time-stamp: <2011-06-20 10:59:28 Monday by lian>
 
 # @version 1.0
 # @author lian
 
-# ttmkfdir
+# please download chinese fonts from
+# http://code.google.com/p/rcl/downloads/list
+# if you are going to compile chinese
+# documents in latex, then you copy
+# these fonts to ~/.fonts, finally
+# invoke this script to build fonts
+# cache.
+cd ~/.fonts
+
 mkfontscale
 mkfontdir
 fc-cache
