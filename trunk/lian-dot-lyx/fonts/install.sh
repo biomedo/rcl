575a8fcfb9d1fc481a64a1fea642956c4c0bfd6c
WeiDong.Lian@gmail.com <WeiDong.Lian@gmail.com@d73213c8-4dec-0d8a-8c91-9a670c5a0931>
2013-01-26 15:38:07 +0000

    add font settings to support chinese

--- trunk/lian-dot-lyx/fonts/install.sh
+++ trunk/lian-dot-lyx/fonts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Time-stamp: <2011-06-20 10:59:28 Monday by lian>
+# Time-stamp: <2013-01-26 16:29:22 Saturday by lian>
 
 # @version 1.0
 # @author lian
@@ -12,8 +12,18 @@
 # these fonts to ~/.fonts, finally
 # invoke this script to build fonts
 # cache.
+# Norammly if your ubuntu is English version, then for Asian fonts the system will use an ugly Chinese fonts as default.
+# Two settings are available to tackle this issue:
+# 1. sudo ln -s ../conf.avail/69-language-selector-zh-cn.conf
+# 2. sudo nano /etc/fonts/conf.d/65-nonlatin.conf, then find san-serif and add the following line after <prefer>
+#    <family>Microsoft YaHei</family> or <family>WenQuanYi Micro Hei</family>
+#    find momospace and then add <family>Microsoft YaHei</family> or <family>WenQuanYi Micro Hei Mono</family>
+
 cd ~/.fonts
 
 mkfontscale
 mkfontdir
-fc-cache
+fc-cache -fv
+
+cd /etc/fonts/conf.d/
+sudo ln -s ../conf.avail/69-language-selector-zh-cn.conf
